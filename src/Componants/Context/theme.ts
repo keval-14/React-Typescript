5bb4450c9c89a7a0b85e6ef05057b4f764c4ed01
Keval Mangukiya <work.kevalpatel@gmail.com>
2022-06-21 16:14:12 +0530

    first commit

---
+++ src/Componants/Context/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+
+    primary : {
+
+        main: "#3f51b5",
+        text : "#fff",
+
+    },
+
+    secondary : {
+
+        main: "#f50057",
+        text: "#fff",
+
+    },
+
+
+}
